8eca31cee5233dc71ba27da658f55b4edb41dce0
Vlada <vladimir.vicentic569@gmail.com>
2025-09-23 22:07:09 +0200

    Dodat glavni deo home stranice ispod menija

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,11 @@ import { Menu } from "./Menu";
 import '../css/home.css'
 import { pageType } from '../components/Menu'
 import { dataUser } from '../modules/User';
+import arrowUpRight from '../img/arrow-up-right.png'
+import { useNavigate } from "react-router-dom";
 
 export function Home() {
+    let navigate = useNavigate();
     let currentUserJSON = localStorage.getItem("currentUser")
     let currentUser: dataUser;
 
@@ -16,7 +19,14 @@ export function Home() {
     return (
         <div>
             <Menu page={pageType.Home} user={currentUser}></Menu>
+            <div id="content-Home">
+                <div id="title-home">QUIZIX</div>
+                <div id="subtitle-home">Test your knowledge in a fun way!</div>
+                <div id="button-home" onClick={() => navigate('/quizzes')}>Try our quizzes <img src={arrowUpRight} id='img-home-button' ></img></div>
+            </div>
         </div>
+
+
 
     );
 }
