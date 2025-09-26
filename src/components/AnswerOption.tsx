62da88f7281db4b365751f1412ddf0c5fd27bf89
Vlada <vladimir.vicentic569@gmail.com>
2025-09-26 17:02:04 +0200

    Razvoj strukture stranice sa pojedinacnim pitanjem kviza

---
+++ src/components/AnswerOption.tsx
@@ -0,0 +1,10 @@
+import '../css/answerOption.css'
+
+
+export function AnswerOption() {
+    return (
+        <div className="answer-option">
+
+        </div>
+    );
+}
--- src/components/SingleQuestion.tsx
+++ src/components/SingleQuestion.tsx
@@ -1,3 +1,56 @@
+import { Menu, pageType } from "./Menu";
+import { dataUser } from "../modules/User";
+import '../css/singleQuestion.css'
+import { AnswerOption } from "./AnswerOption";
+import arrowLeft from "../img/arrow-left.png"
+import arrowRight from "../img/arrow-right.png"
+
+
 export function SingleQuestion() {
-    return <div>SingleQuestion</div>
+
+    let currentUserJSON = localStorage.getItem("currentUser")
+    let currentUser: dataUser;
+
+    if (currentUserJSON != null) {
+        currentUser = JSON.parse(currentUserJSON)
+    }
+    else
+        return <div></div>
+
+    return (
+        <div>
+            <Menu user={currentUser} page={pageType.Quizzes} />
+            <div id="single-question-content">
+                <div id="topPart"><div>TOPIC OF QUIZ: GEOGRAPHY</div> <div>Time left: 9:23</div></div>
+                <div id="questionANDanswersContainer">
+                    <div id="questionContainer">1. What is the name of capital of Slovenia?</div>
+                    <div id="answerOptionsContainer">
+                        <div className="answerOptionRow">
+                            <AnswerOption />
+                            <AnswerOption />
+                        </div>
+
+                        <div className="answerOptionRow">
+                            <AnswerOption />
+                            <AnswerOption />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div id="nextQuestion">
+                <div className="currentQuestionNumber">2/10</div>
+                <img className="nextArrow" src={arrowRight}></img>
+                <div className="currentQuestionText">NEXT</div>
+            </div>
+
+            <div id="previousQuestion">
+                <div className="currentQuestionNumber">2/10</div>
+                <img className="nextArrow" src={arrowLeft}></img>
+                <div className="currentQuestionText">PAST</div>
+            </div>
+
+
+        </div>
+    );
 }
